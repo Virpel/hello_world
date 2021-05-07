9efdf27a3c25eb128c279490547a8d77b07ed693
virpel <virpel@gmail.com>
2021-05-07 22:23:13 +0200

    22:22

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-window.onload=function(){
-    document.querySelector('[data-switch-dark]').addEventListener('click', function() {
-        document.body.classList.toggle('dark');
-      });
-  }
+$(document).ready(function() {
+    // Navbar Menu
+    $(".navbar__item").click(function () {
+        $(this).addClass("active");
+        $(".navbar__item").not(this).removeClass("active");
+    });
+
+    // Not Propagation Checkbox
+    $('input[type="checkbox"]').on('change', function() {
+        $('input[type="checkbox"]').not(this).prop('checked', false);
+    });
+
+    // Switch Theme Color 
+    $('#ico-sun').click(function() {
+        $('body').removeClass('dark');
+    });
+
+    $('#ico-moon').click(function() {
+        $('body').addClass('dark');
+    });
+});
+
+
 
